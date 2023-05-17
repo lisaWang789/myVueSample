7b5bf4278c6b4c9deaeb2bb30e62f6efaa464b1a
lisaWang789 <45554380+lisaWang789@users.noreply.github.com>
2023-05-17 19:35:05 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <strong>AAAAA</strong>
+   <HelloWorld msg="aaaaaaaaaaaaaaaaaa"/>
+
 </template>
 
 <script>
